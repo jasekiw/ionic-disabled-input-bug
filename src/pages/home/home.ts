143e20f2cf41e8458004326a14f8fd3908cfbdae
Jason Gallavin <jasongallavin@gmail.com>
2017-07-28 15:01:09 -0400

    update to latest. imitate issue

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,6 +8,57 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   shouldDisable = true;
+
+  public items = [
+    {
+      name: "test",
+      subItems: [
+        { enabled: true, value: "test" },
+        { enabled: true, value: "test" },
+        { enabled: true, value: "test" }
+      ]
+    },
+    {
+      name: "test",
+      subItems: [
+        { enabled: false, value: "" },
+        { enabled: false, value: "" },
+        { enabled: false, value: "" }
+      ]
+    },
+    {
+      name: "test",
+      subItems: [
+        { enabled: false, value: "" },
+        { enabled: false, value: "" },
+        { enabled: false, value: "" }
+      ]
+    },
+    {
+      name: "test",
+      subItems: [
+        { enabled: false, value: "" },
+        { enabled: false, value: "" },
+        { enabled: false, value: "" }
+      ]
+    },
+    {
+      name: "test",
+      subItems: [
+        { enabled: false, value: "" },
+        { enabled: false, value: "" },
+        { enabled: false, value: "" }
+      ]
+    },
+    {
+      name: "test",
+      subItems: [
+        { enabled: false, value: "" },
+        { enabled: false, value: "" },
+        { enabled: false, value: "" }
+      ]
+    }
+  ];
   constructor(public navCtrl: NavController) {
 
   }
